7795783c9b98635a924ce36751e0d756c3def0b6
ZygimantasL <129725364+ZygimantasL@users.noreply.github.com>
2023-09-11 09:41:08 +0300

    starting pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { ContextWrapper } from "./context/GlobalContext";
 import { Home } from './pages/Home';
 import { NoPage } from './pages/404';
 import { PublicLayout } from './layout/Publiclayout';
 
 function App() {
   return (
+    <ContextWrapper>
     <BrowserRouter>
       <Routes>
         <Route Component={PublicLayout}>
         <Route index path='/' element={<Home />}/>
+        <Route index path='/register' element={Register />}/>
+        <Route index path='/login' element={<Login />}/>
         <Route index path='*' element={<NoPage />}/>
         </Route>  
       </Routes>
     </BrowserRouter>
+    </ContextWrapper>
         );
 }
 
